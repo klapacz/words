eb17d4fb300426823e7ae8c8a1f872c8793c2436
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2020-11-10 18:42:00 +0100

    tests setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const { compilerOptions } = require('./tsconfig.json');
+const { defaults: presets } = require('ts-jest/presets');
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const { transform } = presets;
+
+module.exports = {
+    roots: ['<rootDir>/app'],
+    transform,
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>',
+    }),
+};
