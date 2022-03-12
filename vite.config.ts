c40422e946cb0c8fde8f7be2e5e19b43270c05e9
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-03-12 22:27:23 +0100

    cleanup: remove tests files & watchAndRun

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import watchAndRun from '@kitql/vite-plugin-watch-and-run';
 
 export default defineConfig({
-	plugins: [reactRefresh(), tsconfigPaths()],
+	plugins: [
+		reactRefresh(),
+		tsconfigPaths(),
+		watchAndRun([
+			{
+				watch: '**/*.yaml',
+				run: 'npm run build:api',
+				delay: 0,
+			},
+		]),
+	],
 });
