77e08fc4c1e2e5fb77bffdea3f7ad49021f98ad1
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-10 16:09:42 +0200

    add BASE_URL to env

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   vite: {
     ssr: {
       // https://github.com/natemoo-re/astro-icon#setup
